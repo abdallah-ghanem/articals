30ea7d9767f93df367779aed7d6e2539cda05c45
Abdallah <abdallah21basemen@gmail.com>
2024-09-25 17:34:10 +0300

    Add project files.

---
+++ articals/Pages/service-details.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace artical.Pages
+{
+    public class service_detailsModel : PageModel
+    {
+           
+            public void OnGet()
+        {
+        }
+    }
+}
---
+++ articals/Pages/starter-page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace artical.Pages
+{
+    public class starterpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
